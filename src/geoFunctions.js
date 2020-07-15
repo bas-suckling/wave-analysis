4c82e0ac9b1bbdf75739e6b546d408429a4ff1d1
bas.suckling <suckling.w.s@gmail.com>
2020-07-16 11:19:19 +1200

    fix getSpeed function and unix time stamp

--- src/geoFunctions.js
+++ src/geoFunctions.js
@@ -21,14 +21,14 @@ function getSpeedArray(data) {
     let speedArray = []
     for (let i = 0; i < data.length - 1 ; i++) {
         let speed = (geolib.getSpeed({
-            "latitude": data[i].latitude,
-            "longitude": data[i].longitude,
-            "timestamp": (convertUnixTime(data[i].timestamp))
+            "lat": data[i].latitude,
+            "lon": data[i].longitude,
+            "time": (convertUnixTime(data[i].timestamp))
         }, {
-            "latitude": data[i + 1].latitude,
-            "longitude": data[i + 1].longitude,
-            "timestamp": (convertUnixTime(data[i + 1].timestamp))
-        }))      
+            "lat": data[i + 1].latitude,
+            "lon": data[i + 1].longitude,
+            "time": (convertUnixTime(data[i + 1].timestamp))
+        }))
         speedArray.push(speed)
     }
     return speedArray
@@ -38,9 +38,8 @@ function getSpeedArray(data) {
 //converts gpx timestamp to unix time (2020-07-07T03:42:40Z to )
 function convertUnixTime(gpxTimeStamp) {
     let timeStamp = gpxTimeStamp.replace(/\T/g,' ').replace(/\Z/g,'');
-    let unixtTimeStamp = moment(timeStamp).unix().toString()
-    console.log(unixtTimeStamp)
-    return unixtTimeStamp
+    let unixtTimeStamp = moment(timeStamp).unix()
+    return unixtTimeStamp*1000 //miliseconds to seconds
 }
 
 module.exports = {
