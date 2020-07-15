d3a930ef33c966c12092d316607015347ba48f95
bas.suckling <suckling.w.s@gmail.com>
2020-07-16 10:59:35 +1200

    add distance, speed, unix time conversion fuctions

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const {createJSONFile, createTrackPoints} = require('./src/createTrackPointFile')
+const {getTotalDistance, getSpeedArray} = require('./src/geoFunctions')
 
 let gpxFileName = './data/Surf_2020-07-07.gpx'
 let jsonFileName = './data/Surf_2020-07-07.json'
 
+// createTrackPoints(gpxFileName)
+//     .then(data => createJSONFile(jsonFileName, JSON.stringify(data)))
+
 createTrackPoints(gpxFileName)
-    .then(data => createJSONFile(jsonFileName, JSON.stringify(data)))
+    .then(data => console.log(getSpeedArray(data)))
---
+++ src/geoFunctions.js
@@ -0,0 +1,49 @@
+const geolib = require('geolib');
+const moment = require('moment');
+const { unix } = require('moment');
+
+function getTotalDistance(data) {
+    let totalDistance = 0
+    for (let i = 0; i < data.length - 1; i++) {
+        let distanceIncrement = geolib.getDistance({
+            "latitude": data[i].latitude,
+            "longitude": data[i].longitude
+        }, {
+            "latitude": data[i + 1].latitude,
+            "longitude": data[i + 1].longitude
+        })
+        totalDistance += distanceIncrement
+    }
+    return totalDistance
+}
+
+function getSpeedArray(data) {
+    let speedArray = []
+    for (let i = 0; i < data.length - 1 ; i++) {
+        let speed = (geolib.getSpeed({
+            "latitude": data[i].latitude,
+            "longitude": data[i].longitude,
+            "timestamp": (convertUnixTime(data[i].timestamp))
+        }, {
+            "latitude": data[i + 1].latitude,
+            "longitude": data[i + 1].longitude,
+            "timestamp": (convertUnixTime(data[i + 1].timestamp))
+        }))      
+        speedArray.push(speed)
+    }
+    return speedArray
+}
+
+
+//converts gpx timestamp to unix time (2020-07-07T03:42:40Z to )
+function convertUnixTime(gpxTimeStamp) {
+    let timeStamp = gpxTimeStamp.replace(/\T/g,' ').replace(/\Z/g,'');
+    let unixtTimeStamp = moment(timeStamp).unix().toString()
+    console.log(unixtTimeStamp)
+    return unixtTimeStamp
+}
+
+module.exports = {
+    getTotalDistance,
+    getSpeedArray
+}
