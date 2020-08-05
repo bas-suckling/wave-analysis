0b55b8e2826043902c8814539fc50bc463320e19
bas.suckling <suckling.w.s@gmail.com>
2020-08-05 14:26:16 +1200

    created temp backend api routes

---
+++ server/routes/sessions.js
@@ -0,0 +1,15 @@
+const router = require('express').Router()
+
+router.get('/', (req, res) => {
+    //some function to get list of all sessions
+    let tempResponse = "this is a list of all the sessions"
+    res.send(tempResponse)
+})
+
+router.get('/:id', (req, res) =>{
+    // some function to get the specific session data
+    let tempResponse = "this is the data from a single surf session"
+    res.send(tempResponse)
+})
+
+module.exports(router)
