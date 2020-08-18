f10357ab26ff7d03f30300dd963eb724eb3d19a5
willNaylor <williamtnaylor@gmail.com>
2020-08-18 23:03:54 +1200

    created a MAIN() function (mostly for flow clarity); &also tidy ups in dataprocessing & bearinging funtions

--- server/data/dataFunctions/bearingFunctions.js
+++ server/data/dataFunctions/bearingFunctions.js
@@ -11,40 +11,20 @@ function findBeachDirection(bearingArray) {
         let rad = bearing*TO_RAD
         dirX+=Math.sin(rad)/n
         dirY+=Math.cos(rad)/n
-    });
+    })
 
     beachDirection = Math.atan2(dirX,dirY) * TO_DEG
-
     return beachDirection
 }
 
-function findBearingType(beachDirection){
-    if (90 >= beachDirection){
-        if (beachDirection >= 270){
-            let angleRange = "inside360"
-            let minAngle = beachDirection-90
-            let maxAngle = beachDirection+90
-        }else{// >270
-            let angleRange = "outside360"
-            let minAngle = beachDirection - 270
-            let maxAngle = beachDirection - 90
-        }
-    }else{// >90
-        let angleRange = "outside360"
-        let minAngle = beachDirection + 90
-        let maxAngle = beachDirection + 270
-    }
-    return {"angleRange": angleRange,
-            "minAngle": minAngle,
-            "maxAngle": maxAngle}
 
-}
+
 
 function setIsWave(trackPoints, beachDirection) {
-    bearings = findBearingType(beachDirection)
+    beachBearingsPack = findBearingType(beachDirection)
 
     for (let i = 0; i < trackPoints.length-1; i++) {
-        if (trackPoints[i].speed > MIN_SURF_SPEED && bearingCheck(trackPoints[i].bearing,minAngle,maxAngle,angleRange)){
+        if (trackPoints[i].speed > MIN_SURF_SPEED   &&   bearingCheck(trackPoints[i].bearing, beachBearingsPack)){
             trackPoints[i].isWave = true
         }
     }
@@ -52,11 +32,36 @@ function setIsWave(trackPoints, beachDirection) {
 }
 
 
-function bearingCheck(bearing,minAngle,maxAngle,angleRange){
-    if (angleRange == "inside360"){
-        return (minAngle<bearing<maxAngle)
+
+// find if bearing arc crosses the north 360->0 boundary
+// returns this arc bounds ("minangle"/"maxAngle") and the rangetype "insideRange" bool
+function findBearingType(beachDirection){
+    if (90 >= beachDirection){
+        if (beachDirection >= 270){
+            let insideRange = true
+            let minAngle = beachDirection-90
+            let maxAngle = beachDirection+90
+        }else{// >270
+            let insideRange = false
+            let minAngle = beachDirection - 270
+            let maxAngle = beachDirection - 90
+        }
+    }else{// >90
+        let insideRange = false
+        let minAngle = beachDirection + 90
+        let maxAngle = beachDirection + 270
     }
-    else return (bearing<minAngle || maxAngle<bearing)
+    return {"insideRange": insideRange,
+            "minAngle": minAngle,
+            "maxAngle": maxAngle}
+
+}
+
+function bearingCheck(bearing,checks){
+    if (checks.insideRange){
+        return (checks.minAngle < bearing < checks.maxAngle)
+    }
+    else return (bearing < checks.minAngle   ||   checks.maxAngle < bearing)
 }
 
 module.exports = {
--- server/data/dataFunctions/dataProcessingUpdated.js
+++ server/data/dataFunctions/dataProcessingUpdated.js
@@ -8,19 +8,57 @@ const {smoothArray} = require ('./speedFunctions');
 const MAX_SURF_SPEED = 30
 const KMPH_CONVERT = 3.6
 const MIN_SEGMENT_LENGTH = 4
+const SMOOTH_WEIGHT = 1
 
 
-function getMetaData(rawJSONData) {
+//--------------------------------- LOOK HERE 1st ---------------------
+//
+function mainFunk(rawJSONData){
+    let basicProcess=processTrackPoints(rawJSONData)
+    let beachDirection = findBeachDirection(basicProcess.waveDirections)
+    let smoothedData = smoothArray(basicProcess.processedTrackPoints, SMOOTH_WEIGHT)
+    let finalProcess = setIsWave(smoothedData, beachDirection)
+    let segments = createSegments (finalProcess)
+    let meta = getMetaData(rawJSONData,segments)
+
+    return({
+        "meta": meta,
+        "segments": segments
+    })
+}
+//--------------------------------- LOOK HERE 1st ---------------------
+
+
+
+
+function getMetaData(rawJSONData, segments) {
+    totalDist = 0
+    totalDist = 0
+    waveCount = 0
+    segments.forEach(seg=> {
+        totalDist+=seg.dist
+        totalDur+=seg.duration
+        if (seg.isWave){
+            waveCount+=1
+        }
+    })
+
     return{
         "session_name": rawJSONData.session_name,
-        "date": rawJSONData.date
+        "date": rawJSONData.date,
+        //"locationName":(lookup??),
+        "waveCount": waveCount,
+        "totalDist": totalDist,
+        "totalDur:": totalDur
     }
 }
 
-function processTrackPoints(rawJSONData) {
+
+
+function processTrackPoints(rawJSONData) {//main()? or basic process()?
     let processedTrackPoints = []
     let waveDirections = []
-    let totalDistance = 0
+    //let totalDistance = 0
 
     for (let i = 0; i < rawJSONdata.length - 1 ; i++) {
 
@@ -34,7 +72,7 @@ function processTrackPoints(rawJSONData) {
         let p2 = {"lat": lat2,"lng": lng2}
         
         let distanceIncrement = geolib.getPreciseDistance(p1, p2)
-        totalDistance += distanceIncrement
+        //totalDistance += distanceIncrement
         
         let bearing = Math.floor(geolib.getRhumbLineBearing(p1, p2))
 
@@ -68,23 +106,25 @@ function processTrackPoints(rawJSONData) {
         })
     }
 
-    let beachDirection = findBeachDirection(waveDirections)
+    //let beachDirection = findBeachDirection(waveDirections)
 
-    //smooth first
-    let weight = 1
-    processedTrackPoints = smoothArray(processedTrackPoints, weight)  //consider change var names
-    processedTrackPoints = setIsWave(processedTrackPoints)
-    //need to add function to create session segments based on being a wave or not, as well as speed and wave summary totals
+    //processedTrackPoints = smoothArray(processedTrackPoints, SMOOTH_WEIGHT)
+    //processedTrackPoints = setIsWave(processedTrackPoints)
+    //segments = createSegments (processedTrackPoints)
     
-    console.log(processedTrackPoints)//debug only
+    console.log(processedTrackPoints)//debug only?
 
-    return {
-        processedTrackPoints,
-        totalDistance,
-        beachDirection
-    }
+    return {"processedTrackPoints": processedTrackPoints, "waveDirections": waveDirections}
+        //processedTrackPoints,
+        //segments
+        //totalDistance,??
+        //beachDirection??
+    //}
 }
 
+
+//-------------------------------------------------------you were right this won't work corectly in a W-P-W-P-W situation
+//----------------------------------------------------------instead of adding to prev, it will append to next segment! is that okay?
 function createSegments (trackPointsArray) {
     let segmentArray = []
     let segment = []
@@ -92,7 +132,7 @@ function createSegments (trackPointsArray) {
         if (trackPointsArray[i].isWave == trackpoints[i-1].isWave) { //if same
             segment.push(trackPointsArray[i])
         }else{ 
-            if (segment.length > MIN_SEGMENT_LENGTH) { // if prev seg big enough (4 or more)
+            if (segment.length >= MIN_SEGMENT_LENGTH) { // if prev seg big enough (4 or more)
                 segmentArray.push(segment)
                 segment = [trackPointsArray[i]]
             }
@@ -105,9 +145,31 @@ function createSegments (trackPointsArray) {
         }
     }
 
-    // combine adjacent segments with same isWave values
+    // combine adjacent segments with same isWave values ----------------------- NEEDS TEST!!!!!!!!!!!!!!
+    for (let index = 0; index < segmentArray.length-1; index++) {
+        if (segmentArray[i][0].isWave == segmentArray[i+1][0].isWave){//if i&i+1 share isWave
+            segmentArray[i]=segmentArray[i].concat(segmentArray[i+1]) // concate
+            segmentArray.splice(i+1,1)//remove i+1 & shorten segments len
+        }
+    }
 
-    return segmentArray
+    let segmentArrayFull =[]
+    segmentArray.forEach(seg=>{
+        //seg ID ?? use array index? 
+        let isWave=seg[0].isWave
+        let duration = seg[seg.length].time2-seg[0].time1
+        let dist = 0
+        seg.forEach (part=> {
+            dist+=part.dist
+        })
+        segmentArrayFull.push({
+            "isWave": isWave,
+            "duration": duration,
+            "dist": dist,
+            "points": seg
+        })
+       })
+    return segmentArrayFull
 }
 
 function setMaxSpeed(speed) {
