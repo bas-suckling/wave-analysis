8f3675cfb51ad62a274c64faa7b18d4ce6a217bf
Bas Suckling <suckling.w.s@gmail.com>
2020-08-27 20:25:01 +1200

    show temp track points on leafletmap

--- client/components/LeafletMap.jsx
+++ client/components/LeafletMap.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {Map, Marker, Popup, TileLayer} from 'react-leaflet'
+import {Map, Marker, Popup, TileLayer, Polyline} from 'react-leaflet'
+import data from '../../server/data/rawData/tempTrackPoints.json'
 
 class LeafletMap extends React.Component {
 
@@ -11,11 +12,16 @@ class LeafletMap extends React.Component {
     
         return (
             <>  
-                  <Map id="mapid" center={[-40.7411720, 175.1104580]} zoom={12}>
+                  <Map id="mapid" center={[-40.7411720, 175.1104580]} zoom={16}>
                     <TileLayer
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     />
+
+                    <Polyline
+                    positions={data}>
+                        
+                    </Polyline>
                   </Map>
             </>
         )
