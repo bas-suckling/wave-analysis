a509a001afbfc3ce44125aba95ae87ea11ba7fa6
Bas Suckling <suckling.w.s@gmail.com>
2020-09-01 10:41:27 +1200

    import all segments into leaflet

--- client/components/LeafletMap.jsx
+++ client/components/LeafletMap.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import {Map, Marker, Popup, TileLayer, Polyline} from 'react-leaflet'
+import { Map, Marker, Popup, TileLayer, Polyline } from 'react-leaflet'
 //import data from '../../server/data/rawData/tempTrackPoints.json'
 import data from "../../server/data/processedData/2020-08-01_leafMapData.json"
 
 
-        
+
 
 
 
@@ -15,20 +15,28 @@ class LeafletMap extends React.Component {
     }
 
     render() {
-    
+
         return (
-            <>  
-                  <Map id="mapid" center={[-40.7411720, 175.1104580]} zoom={16}>
+            <>
+                <Map id="mapid" center={[-40.7411720, 175.1104580]} zoom={16}>
                     <TileLayer
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     />
 
-                    <Polyline
+                    {data.map((segment, i) => {
+                        return (
+                            <Polyline
+                                key={i}
+                                positions={segment.path}
+                            />)
+                    })}
+
+                    {/* <Polyline
                     positions={data[0].path}>
                         
-                    </Polyline>
-                  </Map>
+                    </Polyline> */}
+                </Map>
             </>
         )
     }
